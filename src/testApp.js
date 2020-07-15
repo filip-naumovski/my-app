148c1cafb89a28c76e3e8d5ae1d630b2ef5e4c9f
Filip Naumovski <filip.naumovski00@live.com>
2020-07-15 20:02:36 +0200

    Setup

---
+++ src/testApp.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function TestApp(){
+    return(
+    <div>
+        <h1>Heading1</h1>
+        <p>Test Paragraph</p>
+        <ul>
+            <li>Element1</li>
+            <li>Element2</li>
+            <li>Element3</li>
+        </ul>
+    </div>
+    )
+}
+
+export default TestApp;
